4ce15d04c621c743068c54d0b2c9a23bf449cab4
John Edwards <jedwards@technicalassociates.com>
2020-03-31 12:49:21 -0400

    First commit

---
+++ regularJavaScript.js
@@ -0,0 +1,97 @@
+document.addEventListener("keydown", function(event){
+   if(event.code === "KeyA") {
+       //console.log("The 'A' key is pressed.")
+       //Apressed();
+       let audio = new Audio("A.mp3");
+       audio.play();
+   }
+   else if(event.code === "KeyS") {
+       //console.log("The 'S' key is pressed.")
+       //SPressed();
+       let audio = new Audio("S.mp3");
+       audio.play();
+   }
+   else if(event.code === "KeyD") {
+       //console.log("The 'D' key is pressed.")
+       //DPressed();
+       let audio = new Audio("D.mp3");
+       audio.play();
+   }
+   else if(event.code === "KeyF") {
+       //console.log("The 'F' key is pressed.")
+       //FPressed();
+       let audio = new Audio("F.mp3");
+       audio.play();
+   }
+   else if(event.code === "KeyG") {
+       //console.log("The 'G' key is pressed.")
+       //GPressed();
+       let audio = new Audio("G.mp3");
+       audio.play();
+   }
+   else if(event.code === "KeyH") {
+       //console.log("The 'H' key is pressed.")
+       //HPressed();
+       let audio = new Audio("H.mp3");
+       audio.play();
+   }
+   else if(event.code === "KeyJ") {
+       //console.log("The 'J' key is pressed.")
+       //JPressed();
+       let audio = new Audio("J.mp3");
+       audio.play();
+   }
+   else if(event.code === "KeyW") {
+       let audio = new Audio("W.mp3");
+       audio.play();
+   }
+   else if(event.code === "KeyE") {
+       let audio = new Audio("E.mp3");
+       audio.play();
+   }
+   else if(event.code === "KeyT") {
+       let audio = new Audio("T.mp3");
+       audio.play();
+   }
+   else if(event.code === "KeyY") {
+       let audio = new Audio("Y.mp3");
+       audio.play();
+   }
+   else if(event.code === "KeyU") {
+       let audio = new Audio("U.mp3")
+       audio.play();
+   }
+   else {
+       console.log("WARNING THAT WAS WRONG")
+   }
+});
+/*
+function APressed() {
+    let aAudio = new Audio("A.mp3");
+    aAudio.play();
+}
+function SPressed() {
+    let sAudio = new Audio("S.mp3");
+    sAudio.play();
+}
+function DPressed() {
+    let dAudio = new Audio("D.mp3");
+    dAudio.play()
+}
+function FPressed() {
+    let fAudio = new Audio("F.mp3");
+    fAudio.play()
+}
+function GPressed() {
+    let gAudio = new Audio("G.mp3");
+    gAudio.play()
+}
+function HPressed() {
+    let hAudio = new Audio("H.mp3");
+    hAudio.play()
+}
+function JPressed() {
+    let jAudio = new Audio("J.mp3");
+    jAudio.play()
+}
+*/
